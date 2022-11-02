7628c53326ecf681a889c0a5f1505912e8dcfb94
KSY0606 <tkddbs9506@naver.com>
2022-11-02 23:37:00 +0900

    예약자 정보, 결제정보 페이지

---
+++ js/stay_book.js
@@ -0,0 +1,23 @@
+$('input[type=radio][name=flexRadioDefault]').on('click', function() {
+
+    var chkValue = $('input[type=radio][name=flexRadioDefault]:checked').val();
+    
+    
+    
+    if (chkValue == '1') {
+    
+    $('.pay1').css('display', 'block');
+    
+    $('.pay2').css('display', 'none');
+    
+    
+    } else if (chkValue == '2') {
+    
+    $('.pay1').css('display', 'none');
+    
+    $('.pay2').css('display', 'block');
+    
+    
+    }
+    
+    });
