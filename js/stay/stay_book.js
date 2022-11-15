80e9dd0ac0eca6e28c2c210c0b95832cc3a566ad
KSY0606 <tkddbs9506@naver.com>
2022-11-15 09:11:41 +0900

    알림창 내용 수정

--- js/stay/stay_book.js
+++ js/stay/stay_book.js
@@ -12,19 +12,19 @@
 });*/
 
 
-function loginCheck(){
+function payCheck(){
 	if (document.login_form.name.value.trim() == ""){
 		alert("이름을 입력해 주세요.");
         document.login_form.name.value = "";
 		document.login_form.name.focus();
 		return false;
 	} else if (document.login_form.tel.value.trim() == ""){
-		alert("전화번호를 입력해주세요.");
+		alert("전화번호를 입력해 주세요.");
         document.login_form.tel.value = "";
 		document.login_form.tel.focus();
 		return false;
 	} else if(!clause_check.checked) {
-        alert("약관 동의를 체크해주세요.");
+        alert("약관 동의를 체크해 주세요.");
         clause_check.focus();
         return false;
     }else {
